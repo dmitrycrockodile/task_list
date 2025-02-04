35d1d7d90769e70658b372bb80c8b3bee194a442
Dmitrij <dmitry.bermas@gmail.com>
2025-02-04 11:57:38 +0100

    Set the permissions for storage and cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # Exit script on any error
 set -e
 
 # Wait for database to be ready
 sleep 5
+
+# Fix permissions for storage and cache
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
 # Run migrations (only if they haven't been run)
 php artisan migrate --force
